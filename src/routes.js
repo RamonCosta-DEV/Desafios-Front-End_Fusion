9b13603f6d230d10b52a880463e6c7da6fd8d979
RamonCosta-DEV <ramonmastrs@gmail.com>
2024-06-05 20:50:10 -0300

    nova atualizacao

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter,Router,Route, Routes} from "react-router-dom";
+import { BrowserRouter,Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import Galeria from "./galeria";
 import Formulario from "./formulario";
